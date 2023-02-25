6bd891c628f8440fb552064b9809b62bc652a990
Tomáš Diblík <dibla.tomas@post.cz>
2023-02-25 01:02:33 +0100

    [Chore] Format proof of concept

--- proof_of_concept.cpp
+++ proof_of_concept.cpp
@@ -17,39 +17,38 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
     if (nCode == HC_ACTION)
     {
-        KBDLLHOOKSTRUCT* p = (KBDLLHOOKSTRUCT*) lParam;
+        KBDLLHOOKSTRUCT* p = (KBDLLHOOKSTRUCT*)lParam;
         if (p->vkCode == VK_LMENU) // VK_LMENU = ALT key
         {
-           switch (wParam){
-
-            case WM_SYSKEYDOWN :{ // use SYSKEYDOWN
+            switch (wParam) {
+            case WM_SYSKEYDOWN: { // use SYSKEYDOWN
                 cout << "Key down" << endl;
 
-                keybd_event(VK_LCONTROL, 0x1D, KEYEVENTF_EXTENDEDKEY | 0, 0 );
-            break;
+                keybd_event(VK_LCONTROL, 0x1D, KEYEVENTF_EXTENDEDKEY | 0, 0);
+                break;
             }
             case WM_KEYUP: // use regular keyup
-             {
+            {
                 cout << "Key up" << endl;
 
-                keybd_event( VK_LCONTROL, 0x1D, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+                keybd_event(VK_LCONTROL, 0x1D, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
                 return 1;
 
-            break;
-             }
+                break;
+            }
             default:
                 wParam = WM_SYSKEYDOWN; // if you do not specify it changes back to alt
                 break;
-           }
+            }
             return 1;
         }
     }
     return CallNextHookEx(hHook, nCode, wParam, lParam);
 }
-int WINAPI WinMain (HINSTANCE hThisInstance, HINSTANCE hPrevInstance, LPSTR lpszArgument, int nCmdShow)
+int WINAPI WinMain(HINSTANCE hThisInstance, HINSTANCE hPrevInstance, LPSTR lpszArgument, int nCmdShow)
 {
 
-   hHook = SetWindowsHookEx(WH_KEYBOARD_LL, &LowLevelKeyboardProc, GetModuleHandle(NULL), NULL);
+    hHook = SetWindowsHookEx(WH_KEYBOARD_LL, &LowLevelKeyboardProc, GetModuleHandle(NULL), NULL);
     if (hHook == NULL)
     {
         cout << "Error" << endl;
@@ -58,7 +57,7 @@ int WINAPI WinMain (HINSTANCE hThisInstance, HINSTANCE hPrevInstance, LPSTR lpsz
 
     MSG messages;
 
-    while (GetMessage (&messages, NULL, 0, 0))
+    while (GetMessage(&messages, NULL, 0, 0))
     {
         TranslateMessage(&messages);
         DispatchMessage(&messages);
