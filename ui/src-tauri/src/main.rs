cc231be83e75ef5b381b7e2b87612f8ac34eb73f
Tomáš Diblík <dibla.tomas@post.cz>
2023-03-26 16:42:17 +0200

    [Wip] Base UI

--- ui/src-tauri/src/main.rs
+++ ui/src-tauri/src/main.rs
@@ -1,6 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{WindowBuilder, WindowUrl};
 use winapi::{shared::minwindef::BYTE, um::winuser::GetKeyboardState};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -17,6 +18,15 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            WindowBuilder::new(app, "core", WindowUrl::App("index.html".into()))
+                .min_inner_size(750.0, 750.0)
+                .max_inner_size(1000.0, 1000.0)
+                .title("KeyXpert")
+                .build()?;
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -3,11 +3,57 @@
 </script>
 
 <main class="container">
-  <h1>Welcome to Tauri!</h1>
+  <div class="header-row">
+    <h1 class="header">KeyXpert</h1>
+    <div class="advanced-settings-button-wrapper">
+      <button class="btn primary">Advanced settings</button>
+    </div>
+  </div>
+  
+  <div class="profile-selection-row">
+    <select id="profile-selector">
+      <option>Ahoje</option>
+      <option>Ahoje</option>
+      <option>Ahoje</option>
+      <option>Ahoje</option>
+      <option>Ahoje</option>
+    </select>
 
-  <p>
-    Click on the Tauri, Vite, and Svelte logos to learn more.
-  </p>
+    <div class="profile-actions">
+      <button class="btn primary">Add profile</button>
+      <button class="btn delete">Delete profile</button>
+    </div>
+  </div>
+  
+  <div class="rebind-list">
+    <div class="sub-heading">Keys</div>
+
+    <div class="rebind-item">
+      <div class="keys-container">
+        <div class="key"> A </div>
+        <!-- <div class="key"> A </div> -->
+        <div class="edit-pen"> 
+          <img src="/public/edit-pen.svg" />
+        </div>
+      </div>
+      <div class="keys-container">
+        <div class="key new"> B </div>
+      </div>
+      <div class="remove-container">
+        <button class="btn delete">Delete</button>
+      </div>
+    </div>
+
+    <div class="rebind-item">
+      <div class="keys-container">
+        <div class="key"> A </div>
+      </div>
+      <div class="keys-container">
+        <div class="key new"> B </div>
+      </div>
+    </div>
+
+  </div>
 
   <div class="row">
     <Greet />
