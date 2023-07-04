96a956dbc279055bfede2a9118ce2d1480920265
Tomáš Diblík <dibla.tomas@post.cz>
2023-07-04 20:53:49 +0200

    [Fix] Dev build

--- ui/src-tauri/build.rs
+++ ui/src-tauri/build.rs
@@ -1,7 +1,8 @@
 fn main() {
-    let mut windows = tauri_build::WindowsAttributes::new();
-    windows = windows.app_manifest(
-        r#"
+    if std::env::var("PROFILE").unwrap() == "release" {
+        let mut windows = tauri_build::WindowsAttributes::new();
+        windows = windows.app_manifest(
+            r#"
 <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
   <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
       <security>
@@ -12,7 +13,10 @@ fn main() {
   </trustInfo>
 </assembly>
 "#,
-    );
-    let attrs = tauri_build::Attributes::new().windows_attributes(windows);
-    tauri_build::try_build(attrs).expect("failed to run build script");
+        );
+        let attrs = tauri_build::Attributes::new().windows_attributes(windows);
+        tauri_build::try_build(attrs).expect("failed to run build script");
+    } else {
+        tauri_build::build()
+    }
 }
